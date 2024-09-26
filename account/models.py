cacdf6ddbea51bc0f43d6d6dca0bee81da6223f0
akintolatech <akintolatechnologies@gmail.com>
2024-09-26 20:26:44 +0100

    User Registration Implementation

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    photo = models.ImageField(
+        upload_to='users/%Y/%m/%d/',
+        blank=True
+    )
+
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
--- ordersys/urls.py
+++ ordersys/urls.py
@@ -25,4 +25,11 @@ urlpatterns = [
     path('', include('account.urls')),
     path('browser/', include('browser.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
