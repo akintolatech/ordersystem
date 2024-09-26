f852be4794531fcf51671bfe42b4b248474946c6
akintolatech <akintolatechnologies@gmail.com>
2024-09-26 01:58:11 +0100

    Login System Implementation

--- account/forms.py
+++ account/forms.py
@@ -2,5 +2,17 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+    )
+    password = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Enter your password',
+                # 'maxlength': '7',
+                # 'pattern': r'\d{2}/\d{4}'
+            }
+        )
+    )
