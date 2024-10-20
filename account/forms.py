26f677601f850ef70855a2faaa8b7366825cec9d
akintolatechnologies <akintolatechnologies@gmail.com>
2024-10-20 11:04:52 +0100

    App Cleansing

--- account/forms.py
+++ account/forms.py
@@ -3,22 +3,22 @@ from django.contrib.auth import get_user_model
 from .models import Profile
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label="",
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
-    )
-    password = forms.CharField(
-        label="",
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Enter your password',
-                # 'maxlength': '7',
-                # 'pattern': r'\d{2}/\d{4}'
-            }
-        )
-    )
-
+# class LoginForm(forms.Form):
+#     username = forms.CharField(
+#         label="",
+#         widget=forms.TextInput(attrs={'placeholder': 'Enter your username'})
+#     )
+#     password = forms.CharField(
+#         label="",
+#         widget=forms.PasswordInput(
+#             attrs={
+#                 'placeholder': 'Enter your password',
+#                 # 'maxlength': '7',
+#                 # 'pattern': r'\d{2}/\d{4}'
+#             }
+#         )
+#     )
+#
 
 class UserRegistrationForm(forms.ModelForm):
 
